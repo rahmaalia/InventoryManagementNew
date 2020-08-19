b8389792fa28e233abfc0125a09a87b01aaa3071
Diamond <rahmaaliaputri27@gmail.com>
2020-08-19 11:08:17 +0700

    firts

---
+++ app/src/main/java/com/rahma/inventorymanagement/MainActivity.java
@@ -0,0 +1,27 @@
+package com.rahma.inventorymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int waktu_loading = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },waktu_loading);
+    }
+}
